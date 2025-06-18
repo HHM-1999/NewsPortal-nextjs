b1d556d00ac17282bf82c17491ab56bf524f521a
HHM-1999 <hhm639069@gmail.com>
2025-06-18 18:39:18 +0600

    loader added in category and archieve page

---
+++ src/app/[catSlug]/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className='loader-section'>
+        <img src={"/assets/media/common/loading.gif"} alt="Loading" title="Loading ....." />
+    </div>
+  )
+}
+
+export default loading
