4aa09c8a406706bd56f435db4f4eaeb688f8f98a
HHM-1999 <hhm639069@gmail.com>
2025-06-23 13:27:45 +0600

    design fixed in skeleton

--- src/components/common/SkeletonSection.jsx
+++ src/components/common/SkeletonSection.jsx
@@ -2,18 +2,21 @@
 import React from 'react';
 
 export default function SkeletonSection() {
-  return (
-    <div className="skeleton-section">
-      <div className="skeleton-header"></div>
-      <div className="skeleton-grid">
-        {[...Array(4)].map((_, i) => (
-          <div key={i} className="skeleton-card">
-            <div className="skeleton-img" />
-            <div className="skeleton-text short" />
-            <div className="skeleton-text" />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div className="container">
+            <div className="skeleton-section">
+                <div className="skeleton-header"></div>
+                <div className="skeleton-grid">
+                    {[...Array(3)].map((_, i) => (
+                        <div key={i} className="skeleton-card">
+                            <div className="skeleton-img" />
+                            <div className="skeleton-text short" />
+                            <div className="skeleton-text" />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+
+    );
 }
